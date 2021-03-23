8a396cc21e1275dc53b031a0601f665784810932
s1ntaxe770r <cyberdev01@protonmail.com>
2021-03-23 15:27:14 +0100

    adds data access object

---
+++ backend/api/db/dao.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"api/models"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// Connect established a databsase connection and performs migrations
+func Connect() *gorm.DB {
+	// use postgres for prod
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+	if err != nil {
+		logrus.Panicf("failed to connect to db. Reason : %s", err.Error())
+	}
+	db.AutoMigrate(&models.Idea{})
+	db.AutoMigrate(&models.Vote{})
+	return db
+}
+
+// Insert creates a new video idea
+func Insert(idea models.Idea, dbcon *gorm.DB) {
+	inserterr := dbcon.Create(&idea).Error
+	if inserterr != nil {
+		logrus.Fatalf("failed to insert %v to database. Reason %s", idea, inserterr.Error())
+	}
+}
+
+func UpdateIdea(idea *models.Idea, dbcon *gorm.DB) {
+	updatedidea := models.Idea{
+		ID:          idea.ID,
+		Description: idea.Description,
+		Votes:       idea.Votes,
+	}
+	updaterr := dbcon.First(idea.ID).Save(updatedidea).Error
+	if updaterr != nil {
+		logrus.Fatalf("unable to upate video idea, reason %s", updaterr.Error())
+	}
+}
+
+func IncVote(idea *models.Idea, dbcon *gorm.DB) {
+	idea.Votes.Count += 1
+	incerr := dbcon.First(idea).Save(&idea).Error
+	if incerr != nil {
+		logrus.Fatal("could not increment vote. Reason %s", incerr.Error())
+	}
+}
