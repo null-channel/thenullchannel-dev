860d41f27b81bd50ff14ce3f6492646e864f7f4f
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-03-24 16:19:56 +0100

    register handlers and create http server

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"api/handlers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/s1ntaxe770r/requel"
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
+	r := mux.NewRouter()
+	r.Use(requel.LogReq)
+	r.HandleFunc("/ideas", handlers.GetIdeas).Methods("GET")
+
+	logrus.Info("server started on 8080")
+	logrus.Fatal(http.ListenAndServe(":8080", r))
 }
