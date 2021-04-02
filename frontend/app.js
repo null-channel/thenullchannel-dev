df8332b55c9052f5418ea4fd6546b27de28814cf
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-02 15:05:10 +0100

    use axios tofetch data

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,16 @@
-
-
-
-
+const endpoint = 'http://localhost:8080/ideas'
 var app = new Vue({
   el: "#app",
-  data: {
-    message: 'the null channel',
+  data () {
+   return {
+	   ideas: [] 
+   }
   },
+  mounted(){
+	axios.get(endpoint,{crossDomain:true})
+	.then(response =>  (this.ideas = response.data))
+	.catch((err) => console.log(err));
+		
+}
 });
 
