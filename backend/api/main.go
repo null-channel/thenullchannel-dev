07ab250ed1bbfc202966c420f80624f85994a799
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-03 23:50:55 +0100

    yet another desperate attempt at fixing cors

--- backend/api/main.go
+++ backend/api/main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"api/handlers"
+	"api/middleware"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/s1ntaxe770r/requel"
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	admin_user = os.Getenv("ADMIN_USER")
+	admin_pass = os.Getenv("ADMIN_PASS")
+)
+
 func main() {
 	r := mux.NewRouter()
 	r.Use(requel.LogReq)
-	r.HandleFunc("/ideas", handlers.GetIdeas).Methods("GET")
+	r.HandleFunc("/ideas", handlers.GetIdeas).Methods(http.MethodGet, http.MethodOptions)
 	r.HandleFunc("/vote", handlers.IncrementVote).Methods("POST")
-
+	r.HandleFunc("/create", middleware.BasicAuth(handlers.InsertIdea, admin_user, admin_pass, "")).Methods("POST")
 	logrus.Info("server started on 8080")
 	logrus.Fatal(http.ListenAndServe(":8080", r))
 }
