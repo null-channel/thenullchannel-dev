a1d10152c28bc6a529c677bc4fe56773bdae2bb6
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-04 12:00:09 +0100

    improve handler response

--- backend/api/handlers/InsertIdea.go
+++ backend/api/handlers/InsertIdea.go
@@ -12,8 +12,11 @@ func InsertIdea(resp http.ResponseWriter, req *http.Request) {
 	// Todo: Data validation
 	json.NewDecoder(req.Body).Decode(&idea)
 	dbcon := db.Connect()
-	db.Insert(idea, dbcon)
+	result, err := db.Insert(idea, dbcon)
+	if err != nil {
+		http.Error(resp, "could not add idea to the database. "+err.Error(), http.StatusBadRequest)
+	}
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusOK)
-	json.NewEncoder(resp).Encode(idea)
+	json.NewEncoder(resp).Encode(result)
 }
