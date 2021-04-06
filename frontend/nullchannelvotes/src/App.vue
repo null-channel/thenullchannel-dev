2907ab41c2fe0ab8fd3f693cac3dfcd59c38ee0b
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-06 03:06:07 +0100

    hacky vue-cli rewrite

---
+++ frontend/nullchannelvotes/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+      <p class="lead-text">What would you like to see next ?</p>
+    <Ideas/>
+  </div>
+</template>
+
+<script>
+import Idea from './components/Idea.vue'
+
+export default {
+  name: 'App',
+  components: {
+   Idea
+  }
+}
+</script>
+
+<style>
+#app .lead-text{
+    color:#8898a4;
+    font-size:5rem;
+    margin-top:7rem;
+    text-align:center;
+    font-family: 'Source Sans Pro', sans-serif;
+
+}
+</style>
